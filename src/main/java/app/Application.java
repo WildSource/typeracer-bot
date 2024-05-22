1190820e37f2d6274de19e02c1700f17fd00b02d
Samuel <ilovetrap123@hotmail.com>
2024-05-22 17:06:03 -0400

    add mig layout dependency

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -10,25 +10,37 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import app.backend.KeyTyper;
+import app.backend.OpticalCharacterReader;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 
-public class Application {
-	private void countdown() {
-		for (int i = 1; i <= 3; i++) {
-			System.out.println("Program starting in " + i + " seconds.");
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+import javax.swing.*;
 
-	public static void main(String[] args) {
-		File testImage = new File("test.png");
-		assert testImage != null : "test file not found";
-	}
+public class Application {
+    private KeyTyper typer;
+    private OpticalCharacterReader ocr;
+
+    public Application() {
+        this.typer = new KeyTyper();
+        this.ocr = new OpticalCharacterReader();
+    }
+
+    private void countdown() {
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Program starting in " + i + " seconds.");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+//        File testImage = new File("test.png");
+//        assert testImage != null : "test file not found";
+    }
 
 }
